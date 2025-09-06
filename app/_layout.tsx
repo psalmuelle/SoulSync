2591374f3daff94cdd0d57d48031c6b27a2f5796
Erinle Samuel <psalmuelle1@gmail.com>
2025-09-06 20:05:32 +0100

    feat: add splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,34 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import Splash from "@/components/ui/Splash";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+  const [isSplashDone, setIsSplashDone] = useState(false);
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
+  if (!loaded || !isSplashDone) {
+    <Splash
+      onAnimationFinish={() => {
+        if (loaded) setIsSplashDone(true);
+      }}
+    />;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
---
+++ components/ui/Splash.tsx
@@ -0,0 +1,29 @@
+import LottieView from "lottie-react-native";
+import { StyleSheet, View } from "react-native";
+
+export default function Splash({
+  onAnimationFinish,
+}: {
+  onAnimationFinish: () => void;
+}) {
+  return (
+    <View style={styles.container}>
+      <LottieView
+        source={require("@/assets/animations/splash.json")}
+        autoPlay
+        loop={false}
+        resizeMode="cover"
+        onAnimationFinish={onAnimationFinish}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fbefe7",
+  },
+});
